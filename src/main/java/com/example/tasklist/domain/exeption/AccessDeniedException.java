93e3f37a7321092a146c3d93e36977e4eeb7a6e6
Anastasia <klansis3000@mail.ru>
2024-06-05 21:18:50 +1000

    start

---
+++ src/main/java/com/example/tasklist/domain/exeption/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.example.tasklist.domain.exeption;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException() {
+        super();
+    }
+}
---
+++ src/main/java/com/example/tasklist/domain/exeption/ResourceMappingException.java
@@ -0,0 +1,7 @@
+package com.example.tasklist.domain.exeption;
+
+public class ResourceMappingException extends RuntimeException {
+    public ResourceMappingException(String message) {
+        super(message);
+    }
+}
